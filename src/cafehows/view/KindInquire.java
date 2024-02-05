6bc7dae9b37fa734d672f5593b0b798f0250968d
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-02-05 09:14:01 +0900

    kind

--- src/cafehows/view/KindInquire.java
+++ src/cafehows/view/KindInquire.java
@@ -8,14 +8,21 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 
 public class KindInquire extends JFrame{
 	private JPanel pEast, pWest, pSouth;
 	private JTextField txtKind;
 	private JButton btnModify, btnCancel, btnDelete, btnAdd;
+	private static JTable jTable;
 	
 	public KindInquire() {
 		this.setTitle("고객-등록/삭제");					
@@ -23,8 +30,26 @@ public class KindInquire extends JFrame{
 		this.setSize(300, 200);
 		
 		//this.getContentPane().add(getPCenter(), BorderLayout.CENTER);
+		this.getContentPane().add(new JScrollPane(getTable()), BorderLayout.WEST);
 		this.getContentPane().add(getPSouth(), BorderLayout.SOUTH);
 	}
+	
+	public static JTable getTable() {
+		if(jTable == null) {
+			jTable = new JTable();
+			final DefaultTableModel tableModel = (DefaultTableModel) jTable.getModel();
+			tableModel.addColumn("종류");
+			
+			DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
+			dtcr.setHorizontalAlignment(SwingConstants.CENTER);
+			TableColumnModel tcm = jTable.getColumnModel();tcm.getColumn(2).setCellRenderer(dtcr);
+			for(int i=0; i<5; i++) tcm.getColumn(i).setCellRenderer(dtcr);
+			
+			
+			jTable.getColumn("번호").setPreferredWidth(5);
+		}
+		return jTable;
+	}	
 	
 	public JPanel getPSouth() {
 		if(pSouth == null) {
@@ -47,6 +72,7 @@ public class KindInquire extends JFrame{
 				public void actionPerformed(ActionEvent e) {
 					
 				}
+				
 			});
 		}
 		return btnModify;
